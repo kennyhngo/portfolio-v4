7514531699d5b7ecc6acbba323e4569cf494cd9c
kennyhngo <kennyngodev@gmail.com>
2025-05-11 17:15:57 -0400

    Adjust layout for vcard portfolio.

---
+++ src/router.ts
@@ -0,0 +1,15 @@
+import { createRouter, createWebHistory } from 'vue-router';
+import Blog from './views/Blog.vue';
+import Home from './views/Home.vue';
+import Resume from './views/Resume.vue';
+
+const routes = [
+  { path: '/', name: 'Home', component: Home },
+  { path: '/resume', name: 'Resume', component: Resume },
+  { path: '/blog', name: 'Blog', component: Blog },
+];
+
+export const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
