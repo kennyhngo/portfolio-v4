62f3a20ee21536a752e0e8d55a2ac83e8db73021
kennyhngo <kennyngodev@gmail.com>
2025-05-13 16:54:16 -0400

    Add pagination and category filtering

--- src/views/Blog.vue
+++ src/views/Blog.vue
@@ -4,9 +4,25 @@
     <h2 class="h2 article-title">Blog</h2>
   </header>
 
+  <!-- List shown on medium and up -->
+  <ul class="filter-list about-text">
+    <li v-for="(category, index) in categories" 
+      :key="index" 
+      :class="{ active: selectedCategory === category}"
+      @click="selectedCategory = category"
+    >{{ category }}</li>
+  </ul>
+
+  <!-- Dropdown shown on small screens -->
+  <select class="filter-dropdown" v-modal="selectedCategory">
+    <option v-for="(category,index) in categories" :key="index" :value="category">
+      {{ category }}
+    </option>
+  </select>
+
   <section class="blog-posts">
     <ul class="blog-posts-list">
-      <li class="blog-post-item" v-for="(blog, index) in blogs" :key="index">
+      <li class="blog-post-item" v-for="blog in paginatedBlogs" :key="blog.title">
         <a :href="blog.link">
           <figure class="blog-banner-box">
             <img :src="blog.image" :alt="blog.alt" loading="lazy" />
@@ -23,16 +39,55 @@
         </a>
       </li>
     </ul>
+
+    <div class="pagination">
+      <button
+        v-for="page in visiblePages"
+        :key="page"
+        :class="{ active: currentPage === page }"
+        @click="currentPage = page"
+      >{{ page }}</button>
+    </div>
   </section>
 </article>
 </template>
 
 
 <script setup lang="ts">
-import { onMounted, ref } from 'vue';
+import { computed, onMounted, ref, watch } from 'vue';
 import { useBlogPosts, type BlogMeta } from '../composables/useBlogPosts';
 
+// Blog filtering
 const blogs = ref<BlogMeta[]>([]);
+const selectedCategory = ref<string>('All');
+const categories = computed(() => {
+  const uniqueCategories = new Set(blogs.value.map(blog => blog.category));
+  return ['All', ...[...uniqueCategories].sort()];
+});
+const filteredBlogs = computed(() => {
+  return selectedCategory.value === 'All' 
+    ? blogs.value 
+    : blogs.value.filter(blog => blog.category === selectedCategory.value);
+});
+
+// Blog pagination
+const currentPage = ref<number>(1);
+const pageSize = 6;
+const pageCount = computed(() => {
+  return Math.ceil(filteredBlogs.value.length / pageSize);
+});
+const paginatedBlogs = computed(() => {
+  const start = (currentPage.value - 1) * pageSize;
+  return filteredBlogs.value.slice(start, start + pageSize);
+});
+watch(selectedCategory, () => {
+  currentPage.value = 1
+});
+
+// Pagination control
+const visiblePages = computed(() => {
+  return Array.from({ length: pageCount.value }, (_, i) => i + 1);
+});
 
 onMounted(async () => {
   const { blogs: loadedBlogs } = await useBlogPosts();
