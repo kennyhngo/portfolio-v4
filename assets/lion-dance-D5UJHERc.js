7cd235ddfce2b233b88afa5f168a27e713cfbd5e
kennyhngo <kennyngodev@gmail.com>
2025-05-13 21:01:58 -0400

    Updates

---
+++ assets/lion-dance-D5UJHERc.js
@@ -0,0 +1,17 @@
+const e=`---
+title: Lion Dance
+date: "2025-05-10"
+category: Hobby
+image: "https://scontent-det1-1.xx.fbcdn.net/v/t39.30808-6/495377817_1236827278453879_2414786110968681456_n.jpg?stp=cp6_dst-jpg_s720x720_tt6&_nc_cat=105&ccb=1-7&_nc_sid=833d8c&_nc_ohc=nWpM3z_AR68Q7kNvwECWjpA&_nc_oc=AdmUBlABo3VLZRXEFZoS_U42og289Ft6ToqEQ48WKAW_-szjoBHYEuXYdu6GluEVlos&_nc_zt=23&_nc_ht=scontent-det1-1.xx&_nc_gid=NBOECQzZNxiF9dWWQaSKsA&oh=00_AfIO-uSjNiDJ-p-vJHilkq7Ghm11gXlUE0DWt6yMn6z0qA&oe=6828B2B9"
+alt: Hello world image
+excerpt: Grand Rapids Lantern Festival lion dance performance.
+slug: lion-dance
+sticky: true
+---
+
+I initially joined the [Golden Tiger Kung Fu Academy](https://gtkfa.com/) to learn tai chi and wushu. After I joined, there were exactly 4 people enrolled in the adult class, thus prompting Master Mei if we wanted to learn and perform in lion dancing. I've always been in the crowd spectating the dance, but never the one perfomring. "Why not?", I said.
+
+<iframe src="https://www.facebook.com/plugins/video.php?height=476&href=https%3A%2F%2Fwww.facebook.com%2FGTKFA%2Fvideos%2F1014205393709569%2F&show_text=false&width=267&t=0" width="267" height="476" style="border:none;overflow:hidden" scrolling="no" frameborder="0" allowfullscreen="true" allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share" allowFullScreen="true"></iframe>
+
+I'm the one in the yellow lion costume 🙂
+`;export{e as default};
