99b3c252fac4721c9574dcb1852fb9776672857f
kennyhngo <kennyngodev@gmail.com>
2025-06-05 17:43:42 -0400

    Updates

---
+++ assets/carelinc-pickleball-CZl04T8c.js
@@ -0,0 +1,35 @@
+const n=`---
+title: CareLinc Pickleball Tournament
+date: "2024-09-16"
+category: Hobby
+image: /thumbnails/carelinc-pickleball.png
+alt: Kenny (me) and Luis (co-worker) competing in 3.0+ pickleball tournament.
+excerpt: CareLinc pickleball Corewell fundraiser tournament.
+slug: carelinc-pickleball
+sticky: true
+---
+
+CareLinc is partnered with the Corewell Health Foundation for 3 years now (as of writing this post), and one way we get involved is by participating in their events. This time, I am happy to represent CareLinc in Corewell's annual pickleball tournament. Although we didn't win, it was a good learning experience as my first tournament.
+
+![Blog post from company's newsboard](/hobby/carelinc-pickleball/newsboard.png)
+
+---
+
+## Image Transcription
+
+![Luis DeKryger (Sales) & Kenny Ngo (IT Dept)](/hobby/carelinc-pickleball/luis-and-kenny.png)
+
+Good morning Kenny & Luis,
+
+Thank you both for participating in our annual Pickleball tournament. You represented CareLinc wonderfully!
+
+Having been part of the Corewell Health Foundation for the past three years, I'm incredibly proud of the initiatives we support. The funds raised from this tournament go directly providing local high school students with free access to mental healthcare during the school day, ensuring support is available to all students, regardless of their insurance coverage.
+
+I understand weekends can be busy, so I truly appreciate you taking the time to join us.
+
+Warm Regards,
+
+Shawna Carlson | Director of Rehab Services
+
+![Kenny and Luis competing in Corewell Health's annual pickleball tournament.](/hobby/carelinc-pickleball/playing-pickleball.png)
+`;export{n as default};
